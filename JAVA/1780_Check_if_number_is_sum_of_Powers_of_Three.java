b1d5a1287cc788a7d4634422053aa3de1514ef5a
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2025-03-04 20:30:09 +0530

    1780_Check_if_number_is_sum_of_Powers_of_Three.java

---
+++ JAVA/1780_Check_if_number_is_sum_of_Powers_of_Three.java
@@ -0,0 +1,16 @@
+// Problem Number: 1780
+
+// Check if number is a Sum of Powers of Three.
+
+class Solution {
+  public boolean checkPowersOfThree(int n) {
+    while (n > 1) {
+       int r = n % 3;
+      if (r == 2)
+        return false;
+      n /= 3;
+    }
+
+    return true;
+  }
+}
