2ee0e4b887601bbf8f4c699ba9df48e66ec2ecaa
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2025-03-18 19:25:10 +0530

    2401_Longest_Nice_Subarray.java

---
+++ JAVA/2401_Longest_Nice_Subarray.java
@@ -0,0 +1,19 @@
+// Problem No: 2401
+
+// Longest Nice Subarray
+
+class Solution {
+  public int longestNiceSubarray(int[] nums) {
+    int ans = 0;
+    int used = 0;
+
+    for (int l = 0, r = 0; r < nums.length; ++r) {
+      while ((used & nums[r]) > 0)
+        used ^= nums[l++];
+      used |= nums[r];
+      ans = Math.max(ans, r - l + 1);
+    }
+
+    return ans;
+  }
+}
