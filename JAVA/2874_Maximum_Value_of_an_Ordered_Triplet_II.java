e41c069ef3834cfab3209902967bda2903858d89
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2025-04-03 18:16:44 +0530

    2874_Maximum_Value_of_an_Ordered_Triplet_II.java

---
+++ JAVA/2874_Maximum_Value_of_an_Ordered_Triplet_II.java
@@ -0,0 +1,19 @@
+// Problem Number: 2874.
+
+// Maximum Value of an Ordered Triplet II.
+
+class Solution {
+  public long maximumTripletValue(int[] nums) {
+    long ans = 0;
+    int maxDiff = 0;
+    int maxNum = 0; 
+
+    for (final int num : nums) {
+      ans = Math.max(ans, (long) maxDiff * num); 
+      maxDiff = Math.max(maxDiff, maxNum - num); 
+      maxNum = Math.max(maxNum, num);  
+    }
+
+    return ans;
+  }
+}
