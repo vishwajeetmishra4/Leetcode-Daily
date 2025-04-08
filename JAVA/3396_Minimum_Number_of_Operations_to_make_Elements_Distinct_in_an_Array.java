bf3ac5b8a88561fb017ac9bf82b0fda9e8e6b740
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2025-04-08 20:43:34 +0530

    Create 3396_Minimum_Number_of_Operations_to_make_Elements_Distinct_in_an_Array.java

---
+++ JAVA/3396_Minimum_Number_of_Operations_to_make_Elements_Distinct_in_an_Array.java
@@ -0,0 +1,13 @@
+// Problem Number: 3396
+
+// Minimum Number of operations to make Elements Distinct in an Array.
+
+class Solution {
+  public int minimumOperations(int[] nums) {
+    Set<Integer> seen = new HashSet<>();
+    for (int i = nums.length - 1; i >= 0; --i)
+      if (!seen.add(nums[i]))
+        return (i + 1 + 2) / 3; // ceil((i + 1) / 3)
+    return 0;
+  }
+}
